f172d930db60ee350ec99ae401099dc6e6e91ffc
fatihsimssek <mm.fatihsimsek@gmail.com>
2024-06-26 23:10:34 +0300

    ömer yazı

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Archis Academy fatih
+          Archis Academy Ömer
         </p>
         <a
           className="App-link"
